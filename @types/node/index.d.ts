4cee4b62a90225b16396039838172f65271d44b7
hungphan <colorvisavn@gmail.com>
2018-06-18 20:52:09 +0800

    Remove @types

--- @types/node/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "http" {
-  export class MicroIncomingMessage extends IncomingMessage {
-    public params: { readonly [key: string]: string };
-    public query: { readonly [key: string]: string };
-  }
-}
