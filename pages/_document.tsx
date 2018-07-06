0102e012518383b5b63c132c5a5657429e443018
hungphan <colorvisavn@gmail.com>
2018-07-06 14:57:27 +0800

    Add meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,14 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <title>Application</title>
+
+          <link rel="manifest" href="/static/manifest.json" />
+          <link href="/static/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
