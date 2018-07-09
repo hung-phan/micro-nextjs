50a1c1cd8b3935287f7305c1919865f042f17e73
hungphan <colorvisavn@gmail.com>
2018-07-09 20:22:32 +0800

    Update dependencies and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
-const TEST_REGEX = "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$";
-
 module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
-  testRegex: TEST_REGEX,
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   transform: {
     "^.+\\.ts$": "ts-jest",
     "^.+\\.tsx$": "babel-jest",
@@ -16,9 +14,6 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "node", "json"],
   collectCoverage: true,
   collectCoverageFrom: [
-    "pages/**/*.{js,jsx,ts,tsx}",
-    "server/**/*.{js,jsx,ts,tsx}",
-    "share/**/*.{js,jsx,ts,tsx}",
-    "!**/node_modules/**"
+    "{pages,server,share}/**/*.{js,jsx,ts,tsx}"
   ]
 };
