2545f73a034ccbf8d1544db298ccf4988959b74e
hungphan <colorvisavn@gmail.com>
2018-09-07 18:30:28 +0800

    Update dependencies

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
           <title>Application</title>
