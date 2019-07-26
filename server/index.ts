8463b4233a53096669c66080dffca7efc27a1daf
hungphan <colorvisavn@gmail.com>
2019-07-26 22:24:11 +0800

    Add more @types lib

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,7 @@ import { httpServer, nextApplication } from "./application";
 import { configManager } from "./infrastructure/service";
 
 nextApplication.prepare().then(() => {
-  httpServer.listen(configManager.PORT, err => {
-    if (err) {
-      throw err;
-    }
-
-    // tslint:disable-next-line
+  httpServer.listen(configManager.PORT, () => {
     console.log(`> Ready on http://localhost:${configManager.PORT}`);
   });
 });
