0856caf542ed84792012553998618bc054e2dbbf
hungphan <colorvisavn@gmail.com>
2019-11-05 23:02:28 +0800

    Update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,21 +11,21 @@ import { Provider } from "react-redux";
 import { Store } from "redux";
 import createStore from "../share/createStore";
 
-export default withRedux(createStore)(
-  class MyApp extends App<{ store: Store }> {
-    public render() {
-      const { Component, pageProps, store } = this.props;
+class MyApp extends App<{ store: Store }> {
+  public render() {
+    const { Component, pageProps, store } = this.props;
 
-      return (
-        <>
-          <Head>
-            <title>My new cool app</title>
-          </Head>
-          <Provider store={store}>
-            <Component {...pageProps} />
-          </Provider>
-        </>
-      );
-    }
+    return (
+      <>
+        <Head>
+          <title>My new cool app</title>
+        </Head>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </>
+    );
   }
-);
+}
+
+export default withRedux(createStore)(MyApp);
