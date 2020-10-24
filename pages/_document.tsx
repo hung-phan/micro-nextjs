bb9d3ea9a32e88007b2836a608831f581e60b084
hungphan <colorvisavn@gmail.com>
2020-10-25 00:42:48 +0700

    Update dependencies

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,26 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import * as React from "react";
 
 class MyDocument extends Document {
   public render() {
     return (
-      <html>
-      <Head>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="theme-color" content="#000000"/>
+      <Html>
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="theme-color" content="#000000" />
 
-        <link rel="manifest" href="/static/manifest.json"/>
-        <link href="/static/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
-      </Head>
-      <body>
-      <Main/>
-      <NextScript/>
-      </body>
-      </html>
+          <link rel="manifest" href="/static/manifest.json" />
+          <link
+            href="/static/favicon.ico"
+            rel="shortcut icon"
+            type="image/x-icon"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     );
   }
 }
